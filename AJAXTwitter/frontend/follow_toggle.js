3af837f32556bfb606df16e40c9fd30183af4dca
Stephanie Hsu <hsusteph5@gmail.com>
2018-09-26 14:49:46 -0700

    add render to FollowToggle

--- AJAXTwitter/frontend/follow_toggle.js
+++ AJAXTwitter/frontend/follow_toggle.js
@@ -2,14 +2,21 @@ class FollowToggle {
   constructor(el){
     this.$el = $el;
     this.userId = $el.data("user-id");
-    this.followState = $el.data("initial-follow-state")
+    this.followState = $el.data("initial-follow-state");
+    this.render(this.followState);
   }
 
-  // render = function(followState) {
-  //   if(followState === "follow"){
-  //
-  //   }
-  // }
+  render = function(followState) {
+    if(followState === "follow"){
+      return "Unfollow!");
+    } else {
+      return "Follow!";
+    }
+  }
+
+  handleClick = function(event){
+    event.preventDefault()
+  }
 }
 
 
